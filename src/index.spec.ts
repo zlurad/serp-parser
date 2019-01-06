4674d2a2e9a95d9b33d52b48d185c08795088a9a
Bojan Cincur <blueboy6@gmail.com>
2019-01-06 13:39:13 +0000

    Handle nojs Google SERP html pages

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,30 +1,80 @@
 import * as fs from 'fs-extra';
-import { GoogleSERP } from './index';
-
-let google: string;
-let google100: string;
-
-beforeAll(() => {
-  google = fs.readFileSync('test/google.html', { encoding: 'utf8' });
-  google100 = fs.readFileSync('test/google100.html', { encoding: 'utf8' });
-});
+import { GoogleSERP, Result } from './index';
 
 test('GoogleSERP should return empty array on empty html string', () => {
   expect(GoogleSERP('')).toEqual([]);
 });
 
-test('GoogleSERP should return 7 results for test/google.html', () => {
-  expect(GoogleSERP(google)).toHaveLength(7);
+describe('Parsing Google page with 10 resuts', () => {
+  let google: string;
+  let serp: Result[];
+
+  beforeAll(() => {
+    google = fs.readFileSync('test/google.html', { encoding: 'utf8' });
+    serp = GoogleSERP(google);
+  });
+
+  test('serp should have 7 results', () => {
+    expect(serp).toHaveLength(7);
+  });
+  
+  test('3rd result should have url https://domains.google/', () => {
+    expect(serp[2].url).toBe('https://domains.google/');
+  });
 });
 
-test('3rd result from test/google.html should have url https://domains.google/', () => {
-  expect(GoogleSERP(google)[2].url).toBe('https://domains.google/');
+describe('Parsing Google page with 100 resuts', () => {
+  let google: string;
+  let serp: Result[];
+
+  beforeAll(() => {
+    google = fs.readFileSync('test/google100.html', { encoding: 'utf8' });
+    serp = GoogleSERP(google);
+  });
+
+  test('serp should have 98 results', () => {
+    expect(serp).toHaveLength(98);
+  });
+  
+  test('2nd result should have url https://domains.google/', () => {
+    expect(serp[1].url).toBe('https://domains.google/');
+  });
 });
 
-test('GoogleSERP should return 98 results for test/google100.html', () => {
-  expect(GoogleSERP(google100)).toHaveLength(98);
+describe('Parsing nojs Google page with 10 resuts', () => {
+  let google: string;
+  let serp: Result[];
+
+  beforeAll(() => {
+    google = fs.readFileSync('test/google-nojs.html', { encoding: 'utf8' });
+    serp = GoogleSERP(google);
+  });
+
+  test('serp should have 7 results', () => {
+    expect(serp).toHaveLength(7);
+  });
+  
+  test('5th result should have url https://domains.google/', () => {
+    expect(serp[4].url).toBe('https://domains.google/');
+  });
 });
 
-test('2nd result from test/google100.html should have url https://domains.google/', () => {
-  expect(GoogleSERP(google100)[1].url).toBe('https://domains.google/');
+describe('Parsing nojs Google page with 100 resuts', () => {
+  let google: string;
+  let serp: Result[];
+
+  beforeAll(() => {
+    google = fs.readFileSync('test/google100-nojs.html', { encoding: 'utf8' });
+    serp = GoogleSERP(google);
+  });
+
+  test('serp should have 97 results', () => {
+    expect(serp).toHaveLength(97);
+  });
+  
+  test('4th result should have url https://domains.google/', () => {
+    expect(serp[3].url).toBe('https://domains.google/');
+  });
 });
+
+
--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,30 @@ export const GoogleSERP = (html: string) => {
   const $ = cheerio.load(html);
   const results: Result[] = [];
 
-  $('.rc .r > a').each((index, element) => {
-    const result: Result = {
-      position: index + 1,
-      url: $(element).prop('href'),
-    };
+  if ($('body').hasClass('srp')) {
+    $('.rc .r > a').each((index, element) => {
+      const result: Result = {
+        position: index + 1,
+        url: $(element).prop('href'),
+      };
 
-    results.push(result);
-  });
+      results.push(result);
+    });
+  } else if ($('body').hasClass('hsrp')) { 
+    // nojs google html
+    $('#ires ol .g .r a:not(.sla)').each((index, element) => {
+
+      const searchParams = new URLSearchParams($(element).prop('href').replace('/url?',''));
+
+      const result: Result = {
+        position: index + 1,
+        // if there is no q parameter, page is related to google search and we will return whole href for it
+        url: searchParams.get('q') || $(element).prop('href')
+      };
+
+      results.push(result);
+    });
+  }
 
   return results;
 };
